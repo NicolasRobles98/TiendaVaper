87a7bda76bec50018da4b611b8b2f56254756ec9
Nicolas Robles <nicolasrobles98@gmail.com>
2024-11-24 15:26:22 -0300

    Primeras entities

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public abstract class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+
+        public string Surname { get; set; } = string.Empty ;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        public string UserRole { get; set; }
+    }
+}
---
+++ src/Infrastructure/Data/BaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        public BaseRepository() { }
+        public T add(T entity)
+        {
+            return entity;
+        }
+
+    }
+}
